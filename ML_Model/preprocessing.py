51be8605a577bff5c8d17031faf26b1c0986cfaf
emlambda <vianie@rpi.edu>
2023-12-05 03:04:33 -0500

    added comments/updated code/added requirements

--- ML_Model/preprocessing.py
+++ ML_Model/preprocessing.py
@@ -1,57 +1,51 @@
 import string
-import pandas as pd
-import numpy as np
-import spacy
-import math
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+import spacy
+import numpy as np
+import pandas as pd
 
-nlp = spacy.load('en_core_web_sm')
-stop_words = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 
-    'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 
-    'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 
-    'these', 'they', 'this', 'to', 'was', 'will', 'with'
-]
-data = pd.read_csv("./Dataset/train_snli.csv",sep = "\t",header=0,names = ['Phrase','Suspicious','Class'])
-#data provided by https://www.kaggle.com/code/mpwolke/plagiarism-mit-detection
-data = data.dropna()[:4000]
-
-words={}
-for text in range(len(data)):
-    phrase = data['Phrase'].iloc[text]
-    sus = data['Suspicious'].iloc[text]
-    phrase = phrase.lower()
-    sus = sus.lower()
+def preprocess(data):
+    # Load English dictionary
+    nlp = spacy.load('en_core_web_sm')
     
-    phrase_ = nlp(phrase)  #stripping unnecesary info
-    phrase_words = [word.text for word in phrase_ ]
-    phrase_words = list(filter(("").__ne__,[word.translate(str.maketrans('', '', string.punctuation)) for word in phrase_words]))
-    sus_ = nlp(sus)
-    sus_words = [word.text for word in sus_ ]
-    sus_words = list(filter(("").__ne__,[word.translate(str.maketrans('', '', string.punctuation)) for word in sus_words]))
-    data['Phrase'].iloc[text] = " ".join(phrase_words)
-    data['Suspicious'].iloc[text] = " ".join(sus_words)
+    # Define stopwords
+    stop_words = [
+        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 
+        'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 
+        'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 
+        'these', 'they', 'this', 'to', 'was', 'will', 'with'
+    ]
     
-#define chunks for vectorization
-def chunk(phrase):
-    chunks = [chunk.text for chunk in nlp(phrase).noun_chunks]
-    return chunks
+    # Clean text
+    for text in range(len(data)):
+        phrase = data['Phrase'].iloc[text]
+        sus = data['Suspicious'].iloc[text]
+        phrase = phrase.lower()
+        sus = sus.lower()
+        phrase_ = nlp(phrase)  #stripping unnecesary info
+        phrase_words = [word.text for word in phrase_ ]
+        phrase_words = list(filter(("").__ne__,[word.translate(str.maketrans('', '', string.punctuation)) for word in phrase_words]))
+        sus_ = nlp(sus)
+        sus_words = [word.text for word in sus_ ]
+        sus_words = list(filter(("").__ne__,[word.translate(str.maketrans('', '', string.punctuation)) for word in sus_words]))
+        data['Phrase'].iloc[text] = " ".join(phrase_words)
+        data['Suspicious'].iloc[text] = " ".join(sus_words)
+    
+    # Split text into noun_chunks
+    def chunk(phrase):
+        chunks = [chunk.text for chunk in nlp(phrase).noun_chunks]
+        return chunks
 
-tfidf_vectorizer = TfidfVectorizer(tokenizer = chunk,norm = None)
-X = tfidf_vectorizer.fit_transform(data["Phrase"] + " " + data["Suspicious"])
-y = data["Class"]
+    # Vectorize data
+    tfidf_vectorizer = TfidfVectorizer(tokenizer = chunk,norm = None)
+    tfidf_vectorizer.fit(data["Phrase"] + " " + data["Suspicious"])
+    sus=tfidf_vectorizer.transform(data["Suspicious"])
+    act = tfidf_vectorizer.transform(data["Phrase"])
+    print((act.toarray()))
+    X = [np.matrix(sus.toarray()),np.matrix(act.toarray())]
+    y = data["Class"]
+    return X,y,tfidf_vectorizer.vocabulary_
+        
 
-model = LogisticRegression()
-model.fit(X, y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred)
 
-print("Accuracy:", accuracy)
-print("Classification Report:")
-print(classification_rep)
 
--- ML_Model/train_model.py
+++ ML_Model/train_model.py
@@ -1,26 +1,22 @@
-import pandas as pd
-import os
-print(os.getcwd())
 from preprocessing import preprocess
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
 import pickle
-from flask import Flask
-from flask import request
 import numpy as np
 import random
-from flask import jsonify
-import ast
-##idk why i never pushed this to the repo...
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("./Dataset/train_snli.csv",sep = "\t",header=0,names = ['Phrase','Suspicious','Class'])
-#data provided by https://www.kaggle.com/code/mpwolke/plagiarism-mit-detection
-data = data.dropna()[:10]
+# Data provided by https://www.kaggle.com/code/mpwolke/plagiarism-mit-detection
+data = data.dropna()[:1500]
 
+#document -> tf-idf vectors
 X,y,vocab = preprocess(data)
 
+# Compare each suspicious document to every other document in database
 X = [[X[0][x],X[0][d],y[x]] if d==x else [X[0][x],X[0][d],0] for d in range(len(X[1])) for x in range(len(X[0]))]
 y = [X[x][-1] for x in range(len(X))]
 X = [[np.array(X[x][:-1]).astype(np.float32) for x in range(len(X))]]
@@ -42,46 +38,61 @@ random.shuffle(shuf)
 X = X_[shuf]
 y = y_[shuf]
 
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-logreg_model = LogisticRegression()
-logreg_model.fit(X_train, y_train)
-y_pred = logreg_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred)
+# Define model
+tree_model = DecisionTreeClassifier()
 
-print("Accuracy:", accuracy)
-print("Classification Report:")
-print(classification_rep) #save trained model
-pickle.dump(logreg_model, open("./trained_models/logistic_regression.sav", 'wb'))
+# Fit model to data
+tree_model.fit(X_train, y_train) #reached an accuracy of 80.62%
 
-tree_model = DecisionTreeClassifier() #reached an accuracy of 98.62%
-tree_model.fit(X_train, y_train)
+# Generate predictions
 y_pred = tree_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
 
-print("Accuracy:", accuracy)
+print("Accuracy of test pre-pruning:", accuracy)
+print("Classification Report pre-pruning:")
+print(classification_rep)
+
+# Prune tree to reduce overfitting
+path = tree_model.cost_complexity_pruning_path(X_train, y_train)
+ccp_alphas, impurities = path.ccp_alphas, path.impurities
+
+# Plot the pruning path
+fig, ax = plt.subplots()
+ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
+ax.set_xlabel("effective alpha")
+ax.set_ylabel("total impurity of leaves")
+ax.set_title("Total Impurity vs Effective Alpha for training set")
+plt.savefig('./plot/pruning_path.png')
+
+# Find the optimal value for ccp_alpha
+optimal_ccp_alpha = ccp_alphas[np.argmin(impurities)]
+
+# Set the ccp_alpha parameter
+tree_model.ccp_alpha = (optimal_ccp_alpha)
+
+# Re-fit the tree
+tree_model.fit(X_train, y_train)
+
+# Re-test the tree
+y_pred_train = tree_model.predict(X_train)
+accuracy = accuracy_score(y_train, y_pred_train)
+classification_rep = classification_report(y_train, y_pred_train)
+
+print("Accuracy on train:", accuracy)
 print("Classification Report:")
 print(classification_rep)
 
-pickle.dump(tree_model, open("./trained_models/decision_tree.sav", 'wb'))
+y_pred = tree_model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
 
-app = Flask(__name__)
+print("Accuracy on test:", accuracy)
+print("Classification Report:")
+print(classification_rep)
 
-@app.route('/isAlive')
-def index():
-    return "true"
+pickle.dump(tree_model, open("decision_tree.sav", 'wb'))
 
-@app.route('/prediction/api/v1.0/some_prediction', methods=['GET'])
-def get_prediction():
-    feature1 = float(request.args.get('f1'))
-    feature2 = float(request.args.get('f2'))
-    feature3 = float(request.args.get('f3'))
-    loaded_model = pickle.load(open('some_model.pkl', 'rb'))
-    prediction = loaded_model.predict([[feature1, feature2, feature3]])
-    return jsonify({"result" : ast.literal_eval(str(prediction[0]))})
-   
-if __name__ == '__main__':
-    app.run(port=5001,host='0.0.0.0')        
-    #app.run(debug=True)
